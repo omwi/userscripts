d39ed5a35ecc7088f970cf5ea26b7924d1661c0a
omwi <bsuiromwi@gmail.com>
2025-11-05 20:20:07 +0300

    feat: add anilist.co open-challenge-editor script

---
+++ anilist/open-challenge-editor/open-challenge-editor.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         anilist: Open Challenge Editor
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  opens awc.moe challenge editor
+// @author       omwi
+// @match        https://anilist.co/forum/thread/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=anilist.co
+// @require      file://C:\Projects\UserScripts\anilist\open-challenge-editor\open-challenge-editor.user.js
+// ==/UserScript==
---
+++ anilist/open-challenge-editor/open-challenge-editor.user.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         anilist: Open Challenge Editor
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  opens awc.moe challenge editor
+// @author       omwi
+// @match        https://anilist.co/forum/thread/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=anilist.co
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const EDITOR_URL = "https://awc.moe/challenges/editor";
+
+  document.addEventListener("keyup", (event) => {
+    if (event.ctrlKey && event.key === " ") {
+      const [threadId, commentId] = getIds(window.location.href);
+      openInEditor(threadId, commentId);
+    }
+  });
+
+  function openInEditor(threadId, commentId) {
+    window.open(`${EDITOR_URL}?tid=${threadId}&cid=${commentId}`);
+  }
+
+  function getIds(url) {
+    return url
+      .split("/")
+      .map((i) => parseInt(i))
+      .filter((v) => !Number.isNaN(v));
+  }
+})();
