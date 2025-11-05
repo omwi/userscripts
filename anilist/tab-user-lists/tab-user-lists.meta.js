074bf37ef427f84e195c65c0d7cb64427f69f9b2
omwi <bsuiromwi@gmail.com>
2025-11-05 22:22:31 +0300

    feat: add anilist.co tab-user-lists script

---
+++ anilist/tab-user-lists/tab-user-lists.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         anilist: Tab User Lists
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  ctrl+q: switch to next list
+// @author       omwi
+// @match        https://anilist.co/user/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=anilist.co
+// @require      file://C:\Projects\UserScripts\anilist\tab-user-lists\tab-user-lists.user.js
+// ==/UserScript==
---
+++ anilist/tab-user-lists/tab-user-lists.user.js
@@ -0,0 +1,34 @@
+// ==UserScript==
+// @name         anilist: Tab User Lists
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  ctrl+q: switch to next list
+// @author       omwi
+// @match        https://anilist.co/user/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=anilist.co
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  document.addEventListener("keyup", (e) => {
+    if (e.ctrlKey && e.key.toLowerCase() === "q") {
+      clickNextTab();
+    }
+  });
+
+  function clickNextTab() {
+    const tabs = document.querySelectorAll("div.filter-group > span");
+    for (let i = 0; i < tabs.length; i++) {
+      const tab = tabs[i];
+      if (!tab.classList.contains("active")) {
+        continue;
+      }
+      if (i + 1 === tabs.length) {
+        tabs[0].click();
+        continue;
+      }
+      tabs[i + 1].click();
+    }
+  }
+})();
