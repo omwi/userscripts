b9394145f4f891429ee4b3fc37d7b44d29fcc875
omwi <bsuiromwi@gmail.com>
2025-11-05 18:09:58 +0300

    feat: add awc.moe update-code script

---
+++ awc/update-code/update-code.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         awc: Update Code Shortcut
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  ctrl+q: update code button
+// @author       omwi
+// @match        https://awc.moe/challenges/editor*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=awc.moe
+// @require      file://C:\Projects\UserScripts\awc\update-code\update-code.user.js
+// ==/UserScript==
---
+++ awc/update-code/update-code.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         awc: Update Code Shortcut
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  ctrl+q: update code button
+// @author       omwi
+// @match        https://awc.moe/challenges/editor*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=awc.moe
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  document.addEventListener("keyup", (event) => {
+    if (event.ctrlKey && event.key.toLowerCase() === "q") {
+      updateCode();
+    }
+  });
+
+  function updateCode() {
+    const updateButton = document.querySelector("#update-code");
+    updateButton.click();
+  }
+})();
