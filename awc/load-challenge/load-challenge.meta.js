8fc56cd8875b1ba6a22f318b3fded26d758466aa
omwi <bsuiromwi@gmail.com>
2025-11-05 18:26:27 +0300

    feat: add awc.moe load-challenge script

---
+++ awc/load-challenge/load-challenge.meta.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         awc: Load Challenge
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  ctrl+shift+c: load challenge from copied challenge url; load challenge from url params(tid, cid)
+// @author       omwi
+// @match        https://awc.moe/challenges/editor*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=awc.moe
+// @require      file://C:\Projects\UserScripts\awc\load-challenge\load-challenge.user.js
+// ==/UserScript==
---
+++ awc/load-challenge/load-challenge.user.js
@@ -0,0 +1,57 @@
+// ==UserScript==
+// @name         awc: Load Challenge
+// @namespace    https://github.com/omwi
+// @version      0.1
+// @description  ctrl+shift+c: load challenge from copied challenge url; load challenge from url params(tid, cid)
+// @author       omwi
+// @match        https://awc.moe/challenges/editor*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=awc.moe
+// ==/UserScript==
+
+(function () {
+  "use strict";
+
+  const BASE_THREAD_URL = "https://anilist.co/forum";
+
+  window.addEventListener("load", async () => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const threadId = urlParams.get("tid");
+    const commentId = urlParams.get("cid");
+    if (!threadId || !commentId) {
+      return;
+    }
+    const challengeUrl = await getChallengeUrl(threadId, commentId);
+    loadChallenge(challengeUrl);
+  });
+
+  let isShiftKeyCopy = false;
+
+  document.addEventListener("keydown", async (event) => {
+    if (event.ctrlKey && event.shiftKey && event.key.toLowerCase() == "v") {
+      isShiftKeyCopy = true;
+    } else {
+      isShiftKeyCopy = false;
+    }
+  });
+
+  window.addEventListener("paste", async (event) => {
+    if (!isShiftKeyCopy) {
+      return;
+    }
+    event.preventDefault();
+    const clipboardData = event.clipboardData;
+    const textContent = clipboardData.getData("text/plain");
+    loadChallenge(textContent);
+  });
+
+  async function getChallengeUrl(threadId, commentId) {
+    return `${BASE_THREAD_URL}/thread/${threadId}/comment/${commentId}`;
+  }
+
+  async function loadChallenge(challengeUrl) {
+    const urlInput = document.querySelector("#challenge-post-url");
+    const loadChallengedButton = document.querySelector("#load-challenge-button");
+    urlInput.value = challengeUrl;
+    loadChallengedButton.click();
+  }
+})();
